4fe9bcdafc3528d13937293896d6e2ee3c35a5fd
Nikodem <upior126@gmail.com>
2023-04-30 17:11:26 +0200

    new views!

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path("", views.index, name="index"),
+    # /polls/id/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # /polls/id/results/
+    path("<int:question_id>/results", views.results, name="results"),
+    # /polls/id/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
+# /polls/
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# /polls/id
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+# /polls/id/results/
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+# /polls/id/vote/
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
